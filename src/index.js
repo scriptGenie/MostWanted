9229acd857611853406ab4a2a216cbe3db02566a
scriptGenie <scriptGenie@protonmail.com>
2024-04-25 04:17:41 -0400

    fleshing out concept + css

--- src/index.js
+++ src/index.js
@@ -18,13 +18,60 @@ async function scrapeEachPage() {
 
         // get each person from page
         for (let j = 0; j < 20; j++) {
-            allThePeople.push(testContainer[whichPage].items[j].title);
+            let personTitle = testContainer[whichPage].items[j].title
+            let personDesc = testContainer[whichPage].items[j].details
+
+            personTitle = strip(personTitle);
+            personDesc = strip(personDesc);
+
+            if (personTitle == 'null' || personTitle.trim() == '') {
+                personTitle = `<span id="Title_${i}_${j}" class="Description">NO Title PROVIDED</span>`
+            } else {
+                let tempTitle = `<span id="Title_${i}_${j}" class="Title">${personTitle}</span>`
+                personTitle = tempTitle
+            };
+
+
+            if (personDesc == 'null' || personDesc.trim() == '') {
+                personDesc = `<span id="Desc_${i}_${j}" class="Description">NO DESC PROVIDED</span>`
+            } else {
+                let tempDesc = `<span id="Desc_${i}_${j}" class="Description">${personDesc}</span>`
+                personDesc = tempDesc
+            };
+
+            let getBlock = personTitle + personDesc
+
+            allThePeople.push(getBlock);
+            // allThePeople.push(testContainer[whichPage].items[j].title);
         }
 
         // first argument is page number for div id, second is what we want to stuff in the html
+        // console.log(allThePeople)
+
         addElement(i, allThePeople);
     }
 }
+
+// Working so far
+// function stripHtml(html)
+// {
+//    let tmp = document.createElement("DIV");
+//    tmp.innerHTML = html;
+//    return tmp.textContent || tmp.innerText || "";
+// }
+
+
+function strip(str) {
+    const parsedHTML = new DOMParser().parseFromString(str, "text/html");
+    const text = parsedHTML.body.textContent;
+  
+    if (/(<([^>]+)>)/gi.test(text)) {
+      return stripHTML(text);
+    }
+  
+    return text || "";
+  }
+
 
 function addElement(inc, allThePeople) {
     // inc: integer value of current loop, allThePeople: array containing each record for current page
@@ -34,11 +81,11 @@ function addElement(inc, allThePeople) {
     let divID = 'div' + inc;
     newDiv.id = divID;
 
-    let moreHTMLBuilder = `<span id="Page_Title_${inc}" class="Page_Title">##~ PAGE ${inc} ~##</span><br>`;
+    let moreHTMLBuilder = `<span id="Page_Title_${inc}" class="Page_Title">PAGE ${inc}</span>`;
 
     // On Each Page
     allThePeople.forEach((element, index) => {
-        moreHTMLBuilder += `<p id="ITEM_${inc}_${index}" class="Item">ITEM ${index}: ${element}</p><br>`;
+        moreHTMLBuilder += `<p id="ITEM_${inc}_${index}" class="Item">${element}</p>`;
     });
 
     moreHTMLBuilder += '<br><br>';
